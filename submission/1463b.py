e643beb5eb8857159362869b31d5c838c037eb3f
SleepingJempa <alirezaopmc@gmail.com>
2021-04-12 13:32:56 +0430

    gitfix

--- submission/1463b.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())):
-    n = int(input())
-    a = list(map(int, input().split()))
-
-    for i in range(n):
-        j = 0
-        while not (1 << j) > a[i] / 2: j += 1
-        print((1 << j), end=' ')
-    print()
--- tmp/ali_hacker.py
+++
@@ -1,3 +0,0 @@
-n = int(input())
-a = sorted(list(map(int, input().split())))
-print(sum(a[:n//3]))
